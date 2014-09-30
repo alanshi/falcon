6093545c319c0bd79e77f727643562aeb5053eea
alanshi <alan@wesharetech.com.cn>
2014-10-01 02:29:15 +0800

    add PROJECT_NAME in config.py

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ import os.path
 import base64
 
 #-- application config --
+PROJECT_NAME = 'falco'
 SECRET_KEY = 'NjNkM2RhZDVkODE1Njk1N2NiMmZhNDBhMzMzZWRlZjZmZjYyMjc2ODFkM2JjMzllZWVmN2ZiMDIyMjZiNzcyMA=='
 SITE_COOKIE = 'ZWRkNzNkZTQzYzQ4YzYwZDE1OGQ1YWZmNWRlNmJjYmRkODU3NmRkNGI3ZDQ3NWZhZTZiODkxZWRhNWIzMzg5NA=='
 SITE_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -22,7 +23,7 @@ REST_SESSION_TIMEOUT = 3600*24*30
 
 # default log filename
 LOG_PATH = os.path.join(SITE_PATH, 'log')
-LOG_FILE = os.path.join(LOG_PATH, 'falco.log')
+LOG_FILE = os.path.join(LOG_PATH, '%s.log' % (PROJECT_NAME))
 
 
 #-- mysql db config --
