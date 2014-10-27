ae86990fb408188925d04ac499da241ea44f953a
alanshi <alan@wesharetech.com.cn>
2014-10-28 00:03:25 +0800

    add the base template html files

--- handler/index.py
+++ handler/index.py
@@ -17,6 +17,7 @@ class IndexHandler(base.BaseHandler):
   def get(self):
     logging.info('%s Entering...' % (self.__class__.__name__))
     logging.info('write IndexHandler')
-    self.write('IndexHandler at %s'  % (datetime.datetime.now()))
+    render_str = 'IndexHandler at %s'  % (datetime.datetime.now())
+    self.render('index.html',render_str=render_str)
     logging.info('%s Leaving...' % (self.__class__.__name__))
     return
